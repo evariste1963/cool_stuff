12ce8bbbeb2f5dfdbf6f0644e4e37d5dc2cab72b
evariste1963 <evariste@hotmail.co.uk>
2023-03-02 21:09:12 +0000

    buttons ect

---
+++ src/lib/Button89.svelte
@@ -0,0 +1,44 @@
+<script>
+    // your script goes here
+</script>
+
+
+<a href="/" class="button-89" data-sveltekit-preload-data>Button-89</a>
+
+<style>
+.button-89 {
+	--b: 3px;
+	--s: 0.45em;
+	--color: #242529;
+    width: 10em;
+	margin: 0 4rem;
+	text-decoration: none;
+	padding: calc(0.5em + var(--s)) calc(0.9em + var(--s));
+	color: var(--color);
+	--_p: var(--s);
+	background: conic-gradient(from 90deg at var(--b) var(--b), #0000 90deg, var(--color) 0) var(--_p)
+		var(--_p) / calc(100% - var(--b) - 2 * var(--_p)) calc(100% - var(--b) - 2 * var(--_p));
+	transition: 0.3s linear, color 0s, background-color 0s;
+	outline: var(--b) solid rgba(0, 0, 0, 0);
+	outline-offset: 0.6em;
+	font-size: 16px;
+	cursor: pointer;
+	border: 0;
+	user-select: none;
+	-webkit-user-select: none;
+	touch-action: manipulation;
+}
+
+.button-89:hover,
+.button-89:focus-visible {
+	--_p: 0px;
+	outline-color: var(--color);
+	outline-offset: 0.05em;
+}
+
+.button-89:active {
+	background: var(--color);
+	color: #fff !important;
+}
+
+</style>
---
+++ src/lib/SliderBtn.svelte
@@ -0,0 +1,70 @@
+<script>
+    // your script goes here
+</script>
+
+
+
+   
+    <a href="/" >Slider Button</a>
+
+
+<style>
+
+	 a {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 10em;
+		height: 80%;
+		border-top: solid 2px #fff;
+		border-bottom: solid 2px #fff;
+		border-left: none;
+		border-right: none;
+		font-size: 1.5rem;
+		font-weight: 500;
+		text-decoration: none;
+		padding: 1rem 1rem;
+		border-radius: 0.7rem;
+		margin: 0;
+		color: rgba(255, 255, 255, 0.7);
+		background-color: rgba(97, 108, 110, 0.7);
+		cursor: pointer;
+		transition: color 1s ease;
+		transition: font-weight 1s ease;
+		z-index: 1;
+	}
+
+	/*slide work by changing the origin from left to right*/
+ a::before {
+		border-radius: 0.7rem;
+		content: ' '; /*attr(id)*/
+		display: block;
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		inset: 2px;
+		background: rgba(255, 255, 255, 0.6);
+		z-index: -1;
+		transform: scaleX(0);
+		transform-origin: top right;
+		transition: transform 0.5s ease;
+	}
+
+	 a:hover::before {
+		transform: scaleX(1);
+		transform-origin: top left;
+		/*content: attr(id);*/
+	}
+
+	 a:hover {
+		font-weight: bold;
+		color: #292727;
+		z-index: 1;
+		border-radius: 0.7rem;
+	}
+
+
+</style>
