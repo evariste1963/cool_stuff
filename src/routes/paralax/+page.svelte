2626724cd8f4ef9bbe77cefb8def644815e3c106
evariste1963 <evariste@hotmail.co.uk>
2023-03-13 19:31:33 +0000

    svelte_paralax

---
+++ src/routes/paralax/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	let scroll;
+	let speed = 3
+</script>
+
+<svelte:window bind:scrollY={scroll} />
+
+<h1>{scroll}</h1>
+<section>
+	<div class="box pin" style:transform={`translate3d(0, ${scroll * speed}px, 0)`} style:background="teal" />
+	<div class="box" style:margin-left="20em"/>
+	<div class="box" style:margin-left="100em"/>
+	<div class="box" style:margin-left="40em"/>
+	<div class="box" style:margin-left="80em"/>
+	<div class="box" style:margin-left="60em"/>
+	<div class="box" style:transform={`translate3d(0, ${scroll * speed * -0.2}px, 0)`} style:background="red" style:margin-left="30em"/>
+	<div class="box" style:margin-left="120em"/>
+</section>
+
+
+<style>
+    section {
+        height: 1000em
+    }
+	h1 {
+		position: fixed;
+		z-index: 10;
+	}
+	.box {
+		--size: 150px;
+		height: var(--size);
+		width: var(--size);
+		background: lightGrey;
+		margin-bottom: 1rem;
+	}
+	
+/* 	.pin {
+		position: fixed;
+	} */
+</style>
