d06d6654e7c07bf24223d2da25b84513b2493ff1
evariste1963 <evariste@hotmail.co.uk>
2023-03-24 17:06:29 +0000

    cq at home

--- src/lib/Container/Container.svelte
+++ src/lib/Container/Container.svelte
@@ -2,52 +2,45 @@
   import "./container.css";
 </script>
 
-<h1>Container queries</h1>
+<h1>container queries</h1>
 <div class="post">
   <div class="cont">
     <div class="card">
       <h2>Card title</h2>
       <p
-        >Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        >Donec accumsan ullamcorper diam nec finibus. Etiam et ante justo. Cras ac augue fringilla, 
+        hendrerit dolor ut, porta nisi. Donec vel maximus lectus. Proin consectetur condimentum metus, 
+        eget tincidunt diam molestie vel. Sed libero lorem, hendrerit ac orci vel, malesuada sagittis sem. 
+        Fusce tempor enim at commodo volutpat. Nullam vehicula, sapien quis eleifend vestibulum, neque nunc pharetra nisl, 
+        eget interdum sem felis vitae ex. Phasellus malesuada diam eu risus mattis, sed iaculis lacus vestibulum.
+
+        Praesent sit amet dolor risus. Cras et congue risus. Proin a tortor eu erat tincidunt fringilla. 
+        Nulla facilisi. Nullam nec dolor in diam tempus tincidunt. Vivamus eget arcu tortor. Nunc mattis elit quis egestas mollis.
       </p>
     </div>
 
     <div class="card">
       <h2>Card title</h2>
       <p
-        >Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+        >Curabitur quis dictum nibh, mattis iaculis nibh. In hac habitasse platea dictumst. 
+        Nam accumsan libero non enim euismod, a tincidunt libero blandit. Aliquam erat volutpat. 
+        Integer ut malesuada diam, non molestie diam. Mauris sit amet imperdiet tellus, vel fringilla lorem. 
+        Sed suscipit lacus et orci placerat blandit. Integer pulvinar magna eros, quis ullamcorper lectus aliquam vitae. Mauris id nulla blandit.
+        Curabitur quis dictum nibh, mattis iaculis nibh. In hac habitasse platea dictumst. 
+        Nam accumsan libero non enim euismod, a tincidunt libero blandit. Aliquam erat volutpat. 
+        Integer ut malesuada diam, non molestie diam. Mauris sit amet imperdiet tellus, vel fringilla lorem. 
+        Sed suscipit lacus et orci placerat blandit. Integer pulvinar magna eros, quis ullamcorper lectus aliquam vitae. Mauris id nulla blandit.</p>
     </div>
 
     <div class="card">
       <h2>Card title</h2>
       <p
-        >Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+        >Mauris non tellus vulputate, feugiat erat non, pharetra justo.
+         Proin consequat felis diam, malesuada auctor mi convallis sed. Sed gravida faucibus vulputate.
+          Ut id nunc imperdiet, accumsan lacus volutpat, volutpat lectus. Mauris mauris tortor, congue eu lobortis non, 
+          tristique in turpis. Praesent sit amet ipsum eu risus varius blandit. 
+          Donec blandit ac lorem et dignissim. Nam sem nisl, aliquam a ornare eu, 
+          luctus quis enim. Aenean ut felis in neque congue rutrum. Aliquam at velit.</p>
     </div>
   </div>
 </div>
