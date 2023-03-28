f4e2ae03d367d35ceb09ef28fac461b70968e864
evariste1963 <evariste@hotmail.co.uk>
2023-03-28 15:02:33 +0100

    stop the bubbling if you cangit cmp e

--- src/lib/Container/Container.svelte
+++ src/lib/Container/Container.svelte
@@ -6,38 +6,46 @@
   let detailEl = [];
 
   onMount(() => {
-    detailEl = post.querySelectorAll(".detail p");
+    detailEl = document.querySelectorAll(".detail p");
+
     detailEl.forEach(el => {
       el.classList.add("excerpt");
       el.innerText = el.innerText.substring(0, 300) + " ... read on ->";
       console.log(el.innerText);
     });
-  });
+    document.querySelector(".cont").addEventListener(
+      "click",
+      e => {
+        console.log(e);
 
-  function toggleClass(e) {
-    if (!e.target.classList.contains("excerpt")) {
-      e.target.innerHTML =
-        e.target.innerHTML.substring(0, 300) + " ... read on ->";
-      e.target.classList.toggle("excerpt");
-    } else {
-      e.target.innerHTML = `Donec accumsan ullamcorper diam nec finibus. Etiam et ante justo. Cras
-        ac augue fringilla, hendrerit dolor ut, porta nisi. Donec vel maximus
-        lectus. Proin consectetur condimentum metus, eget tincidunt diam
-        molestie vel. Sed libero lorem, hendrerit ac orci vel, malesuada
-        sagittis sem. Fusce tempor enim at commodo volutpat. Nullam vehicula,
-        sapien quis eleifend vestibulum, neque nunc pharetra nisl, eget
-        interdum sem felis vitae ex. Phasellus malesuada diam eu risus mattis,
-        sed iaculis lacus vestibulum. Praesent sit amet dolor risus. Cras et
-        congue risus. Proin a tortor eu erat tincidunt fringilla. Nulla
-        facilisi. Nullam nec dolor in diam tempus tincidunt. Vivamus eget arcu
-        tortor. Nunc mattis elit quis egestas mollis.`;
-      e.target.classList.toggle("excerpt");
-    }
-  }
+        if (e.target.classList.contains("excerpt")) {
+          e.target.innerHTML = `Donec accumsan ullamcorper diam nec finibus. Etiam et ante justo. Cras
+                                  ac augue fringilla, hendrerit dolor ut, porta nisi. Donec vel maximus
+                                  lectus. Proin consectetur condimentum metus, eget tincidunt diam
+                                  molestie vel. Sed libero lorem, hendrerit ac orci vel, malesuada
+                                  sagittis sem. Fusce tempor enim at commodo volutpat. Nullam vehicula,
+                                  sapien quis eleifend vestibulum, neque nunc pharetra nisl, eget
+                                  interdum sem felis vitae ex. Phasellus malesuada diam eu risus mattis,
+                                  sed iaculis lacus vestibulum. Praesent sit amet dolor risus. Cras et
+                                  congue risus. Proin a tortor eu erat tincidunt fringilla. Nulla
+                                  facilisi. Nullam nec dolor in diam tempus tincidunt. Vivamus eget arcu
+                                  tortor. Nunc mattis elit quis egestas mollis.`;
+
+          e.target.classList.toggle("excerpt");
+        } else {
+          if (e.target.classList.contains("info"))
+            e.target.innerText =
+              e.target.innerText.substring(0, 300) + " ... read on ->";
+          e.target.classList.toggle("excerpt");
+        }
+      },
+      false
+    );
+  });
 </script>
 
 <h1>container queries</h1>
- <!-- put ALL card contents in array of objects-->
+<!-- put ALL card contents in array of objects-->
 <div class="post" bind:this={post}>
   <div class="cont">
     <div class="card">
@@ -46,18 +54,18 @@
       </div>
       <div class="detail">
         <h3 class="subtitle">The Subtitle</h3>
-        <p on:mousedown={toggleClass}> <!-- use event delegation on POST class tcjsc lesson 192 17:20 (bankist)-->
+        <p class="info">
+          <!-- use event delegation on POST class tcjsc lesson 192 17:20 (bankist)-->
           Donec accumsan ullamcorper diam nec finibus. Etiam et ante justo. Cras
-          ac augue fringilla, hendrerit dolor ut, porta nisi. Donec vel maximus
-          lectus. Proin consectetur condimentum metus, eget tincidunt diam
-          molestie vel. Sed libero lorem, hendrerit ac orci vel, malesuada
-          sagittis sem. Fusce tempor enim at commodo volutpat. Nullam vehicula,
-          sapien quis eleifend vestibulum, neque nunc pharetra nisl, eget
-          interdum sem felis vitae ex. Phasellus malesuada diam eu risus mattis,
-          sed iaculis lacus vestibulum. Praesent sit amet dolor risus. Cras et
-          congue risus. Proin a tortor eu erat tincidunt fringilla. Nulla
-          facilisi. Nullam nec dolor in diam tempus tincidunt. Vivamus eget arcu
-          tortor. Nunc mattis elit quis egestas mollis.
+          ac augue fringilla, hendrerit dolor ut, porta nisi. Donec vel maximus lectus.
+          Proin consectetur condimentum metus, eget tincidunt diam molestie vel.
+          Sed libero lorem, hendrerit ac orci vel, malesuada sagittis sem. Fusce
+          tempor enim at commodo volutpat. Nullam vehicula, sapien quis eleifend
+          vestibulum, neque nunc pharetra nisl, eget interdum sem felis vitae ex.
+          Phasellus malesuada diam eu risus mattis, sed iaculis lacus vestibulum.
+          Praesent sit amet dolor risus. Cras et congue risus. Proin a tortor eu
+          erat tincidunt fringilla. Nulla facilisi. Nullam nec dolor in diam tempus
+          tincidunt. Vivamus eget arcu tortor. Nunc mattis elit quis egestas mollis.
         </p>
       </div>
     </div>
@@ -67,7 +75,7 @@
       </div>
       <div class="detail">
         <h3 class="subtitle">The Subtitle</h3>
-        <p on:mousedown={toggleClass}>
+        <p class="info">
           Curabitur quis dictum nibh, mattis iaculis nibh. In hac habitasse
           platea dictumst. Nam accumsan libero non enim euismod, a tincidunt
           libero blandit. Aliquam erat volutpat. Integer ut malesuada diam, non
@@ -90,7 +98,7 @@
       </div>
       <div class="detail">
         <h3 class="subtitle">The Subtitle</h3>
-        <p on:mousedown={toggleClass}>
+        <p class="info">
           Mauris non tellus vulputate, feugiat erat non, pharetra justo. Proin
           consequat felis diam, malesuada auctor mi convallis sed. Sed gravida
           faucibus vulputate. Ut id nunc imperdiet, accumsan lacus volutpat,
