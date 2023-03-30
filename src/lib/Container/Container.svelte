495f0359471bc0282ef6bef17a4387e90108974c
evariste1963 <evariste@hotmail.co.uk>
2023-03-30 14:29:37 +0100

    trt

--- src/lib/Container/Container.svelte
+++ src/lib/Container/Container.svelte
@@ -4,7 +4,7 @@
   import img1 from "../images/img1.jpg";
   import img2 from "../images/img2.jpg";
   import img3 from "../images/img3.jpeg";
-  import img4 from "../images/img4.jpeg";
+  import img4 from "../images/img4.jpg";
 
   let post;
   let detailEl = [];
