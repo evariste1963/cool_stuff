d544650306752db91e2bebdbbf4829c080ba199a
evariste1963 <evariste@hotmail.co.uk>
2023-04-03 14:24:34 +0100

    footer text sizing

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -12,7 +12,7 @@
     margin-top: auto;
     bottom: 0;
     width: 100%;
-    height: 4rem;
+    height: 3em;
     background: rgba(97, 108, 110, 0.88);
     box-shadow: 0px -1px 10px 0 rgb(0 0 0);
     text-align: center;
@@ -30,11 +30,12 @@
   }
   p {
     margin: 0 auto;
+    font-size: clamp(0.7rem, 1cqmin + 0.7rem, 3rem);
   }
 
   .this-me {
     color: rgb(46, 10, 80);
-    font-size: 1.5rem;
+    font-size: clamp(0.7rem, 1.5cqmin + 0.7rem, 3rem);
     font-weight: bold;
   }
 </style>
