677ca4ca2369cdb743fd5e9650a13067350c1c46
evariste1963 <evariste@hotmail.co.uk>
2023-04-03 20:22:55 +0100

    starting with disorder myParalax on scroll

---
+++ src/routes/disorder/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from "svelte";
+
+  let sections
+  let offsets = []
+  let y
+   
+  onMount(() => {
+sections = document.querySelectorAll('.section')
+sections.forEach((section, i) =>{
+     offsets.push(section.getBoundingClientRect().top)
+} )
+console.log(offsets);
+
+
+ })
+
+</script>
+<svelte:window bind:scrollY={y}/>
+{console.log(y)}
+<div  class="first section">first div</div>
+<div class="second section">second div</div>
+<div class="third section">third div</div>
+<div class="forth section">forth div</div>
+
+<style>
+    .section {
+        height:100vh;
+        top:0;
+        margin:0;
+    }
+</style>
