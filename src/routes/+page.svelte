95c631a1aa2b23a0309f543625db979a235b74bc
evariste1963 <evariste@hotmail.co.uk>
2023-04-11 12:22:29 +0100

    added section for CSS Masking/clipping

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,17 +44,19 @@
     <h1>Carousels</h1>
     <h2 style="width:100%; text-align:center">Svelte Carousel</h2>
     <div class="scar" style=" width: 27em; height: auto; margin:0 auto">
-      
-    <SvelteCarousel />
-  </div>
-  <h2 style="width:100%; text-align:center">Slide Carousel</h2>
+      <SvelteCarousel />
+    </div>
+    <h2 style="width:100%; text-align:center">Slide Carousel</h2>
     <SlideShow />
   </div>
+  <div style="height:100vh"
+    ><br /><br /><h1>**** CCC MASK/CLIPPING to go in here? **** </h1></div
+  >
   <div style="height:100vh"
     ><br /><br /><h1>**** Attribute_Selectors to go in here? **** </h1></div
   >
 </div>
-  
+
 <div class="embed">
   <SKEmbed />
 </div>
@@ -66,14 +68,13 @@
     margin: 2em;
     font-size: 1rem;
   }
-  
+
   .embed {
     width: 100%;
     height: 800px;
     margin: 4em auto;
     background-color: rgba(57, 119, 119, 0.9);
   }
-
 
   .btn-container {
     display: flex;
@@ -105,9 +106,9 @@
 
   .slideshows {
     margin: 0 auto;
-    height: auto;
+    height: 100vh;
     padding: 1em 0;
-    border-top: 2px solid rgba(97, 108, 110, 0.7); 
+    border-top: 2px solid rgba(97, 108, 110, 0.7);
     border-bottom: 2px solid rgba(97, 108, 110, 0.7);
   }
 </style>
