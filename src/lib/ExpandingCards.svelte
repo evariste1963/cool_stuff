73a38749a071b25251975ef2d1d4866edbe42929
evariste1963 <evariste@hotmail.co.uk>
2023-04-25 21:57:52 +0100

    starting to add details in highlighted expanding card --> won't remove hidden class??

--- src/lib/ExpandingCards.svelte
+++ src/lib/ExpandingCards.svelte
@@ -1,4 +1,5 @@
 <script>
+  
   const imgsArr = Object.keys(import.meta.glob("$lib/images/**/*.*"));
   let cardsArr = [
     {
@@ -29,29 +30,55 @@
         "Lorem ipsum <strong>dolor</strong> sit amet, consectetur adipiscing elit, <strong>sed</strong> do eiusmod tempor incididunt ut labore et <strong>dolore</strong> magna aliqua. Mauris rhoncus <strong>aenean</strong> vel elit. Nibh sed pulvinar proin gravida hendrerit lectus a. <strong>Semper risus</strong> in hendrerit gravida rutrum quisque non tellus.<br><br>Mi <strong>ipsum</strong> faucibus vitae aliquet nec ullamcorper sit amet. Habitant <strong>morbi</strong> tristique senectus et netus et.Faucibus turpis in eu  <strong>mi </strong> bibendum neque <strong>egestas</strong> congue quisque.",
       image: imgsArr[3],
     },
+    {
+      title: "Day Trips",
+      subTitle: "Out amongst the Lions",
+      details:
+        "Donec accumsan <strong>ullamcorper</strong> diam nec finibus. Etiam et ante justo. Cras ac augue fringilla, <strong>hendrerit</strong> dolor ut, porta nisi.<br><br>Fusce <strong>tempor</strong> enim at commodo volutpat. Nullam vehicula, sapien quis eleifend vestibulum, <strong>neque</strong> nunc pharetra nisl, eget interdum sem felis vitae ex. ",
+      image: imgsArr[0],
+    },
+    {
+      title: "Space Travel",
+      subTitle: "Bathe amid the Stars",
+      details:
+        "Curabitur quis dictum nibh, <strong>mattis</strong> iaculis nibh. In hac habitasse platea dictumst. Nam <strong>accumsan</strong> libero non enim euismod, a <strong>tincidunt</strong> libero blandit. Mauris sit <strong>amet</strong> imperdiet tellus, vel fringilla <strong>lorem</strong>. Sed suscipit lacus et orci placerat blandit. <br><br>Integer pulvinar <strong>magna</strong> eros, quis ullamcorper lectus aliquam vitae. Mauris id nulla blandit. Curabitur quis dictum nibh, mattis iaculis nibh.",
+      image: imgsArr[1],
+    },
+    {
+      title: "Run Wild",
+      subTitle: "Beat the Retreat",
+      details:
+        "Mauris non <strong>tellus</strong> vulputate, feugiat erat non, pharetra justo. Proin consequat <strong>felis diam</strong>, malesuada auctor mi convallis sed. Sed <strong>gravida</strong> faucibus vulputate. Mauris mauris <strong>tortor</strong>,  Praesent sit amet <strong>ipsum</strong> eu risus varius blandit. <br><br>Donec blandit ac lorem et dignissim. Nam sem nisl, aliquam a ornare eu, luctus <strong>quis enim</strong>. Aenean ut felis in neque congue rutrum. Aliquam <strong>at</strong> velit.",
+      image: imgsArr[2],
+    },
+    
   ];
 
   function featureCard(i) {
     const cards = document.querySelectorAll(".gridCard");
+    const detail = document.querySelectorAll("p");
     cards.forEach(card => {
       if (card != cards[i]) card.classList.add("not_featured");
     });
     cards[i].classList.remove("not_featured");
+    detail[i].classList.remove(".hidden")
   }
 </script>
 
 <!-- this can also be done with FLIP -->
 <section id="cardGrid">
-  {#each cardsArr as card, i}
-    <div
+  {#each cardsArr as card , i}
+    <div 
       class="gridCard not_featured"
       on:click={() => featureCard(i)}
       on:keydown={() => featureCard(i)}
+      
       style="display:flex; flex-direction:column;  height: 200px; ; align-items:center; justify-content:center; text-align:center; border: solid 1px #252"
     >
       <h1>{card.title}</h1>
       <img src={card.image} alt={card.title} width="100" height="100" />
       <h2>{card.subTitle}</h2>
+      <p class="details hidden">{card.details}</p>
     </div>
   {/each}
 </section>
@@ -63,13 +90,23 @@
       auto-fit,
       minmax(min(100%, max(10rem, 100%/4)), 1fr)
     );
-    gap: 1.5rem;
-    width: 90vw;
+    gap: 0.5em;
+    width: 60vw;
+    margin: 1em auto;
     grid-row: 2;
   }
+
 
   .gridCard:not(.not_featured) {
     order: -1;
     grid-column: 1 / -1;
+  }
+
+  .details{
+    display:none
+  }
+
+  .details:not(.hidden) {
+    display:flex
   }
 </style>
